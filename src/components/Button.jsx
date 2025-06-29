e4b5b0c485689d674b2478402e9c10bbdf704619
Heera Rana <heerarana526@gmail.com>
2025-06-29 23:40:06 +0530

    Introduce the Glassy effect

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -24,13 +24,13 @@ export const ButtonOutline = ({ href, target, label, icon, classes }) => {
 
     if (href) {
         return (
-            <a href={href} target={target} className={"btn btn-outline " + classes}>
+            <a href={href} target={target} className={"btn btn-outline bg-white/10 backdrop-blur-xl shadow-[inset_0_2px_9px_rgba(255,255,255,0.2),_0_8px_20px_rgba(0,0,0,0.5)] ring-white/10 text-white ring-1 ring-inset" + classes}>
                 {label} {icon ? <span className="material-symbols-rounded" aria-hidden="true">{icon}</span> : undefined}
             </a>
         )
     } else {
         return (
-            <button className={"btn btn-outline " + classes}>
+            <button className={"btn btn-outline  bg-white/10 backdrop-blur-xl shadow-[inset_0_2px_9px_rgba(255,255,255,0.2),_0_8px_20px_rgba(0,0,0,0.5)] ring-white/10 text-white ring-1 ring-inset" + classes}>
                 {label} {icon ? <span className="material-symbols-rounded" aria-hidden="true">{icon}</span> : undefined}
             </button>
         )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
 
                         <div className="flex items-center gap-2 mt-auto">
                             {socialLinks.slice(0, 2).map(({ href, icon }, key) =>
-                                <a key={key} href={href} target="_blank" className="w-9 h-9 grid place-items-center ring-inset ring-2 ring-zinc-50/5 rounded-lg transition-[background-color,color] hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80 reveal-up">
+                                <a key={key} href={href} target="_blank" className="w-9 h-9 grid place-items-center rounded-lg transition-[background-color,color] hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80 reveal-up     bg-white/10 backdrop-blur-xl shadow-[inset_0_1px_3px_rgba(255,255,255,0.2),_0_8px_20px_rgba(0,0,0,0.5)] ring-white/10 text-white ring-1 ring-inset">
                                     {icon}
                                 </a>
                             )}
